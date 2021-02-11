d644909976a8cc33d80d17331306a4d2dffde191
roast247 <77567356+roast247@users.noreply.github.com>
2021-02-11 15:55:22 -0800

    Add files via upload

---
+++ MinecraftSkinGrabber/Form1.Designer.vb
@@ -0,0 +1,193 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New ComponentFactory.Krypton.Toolkit.KryptonLabel()
+        Me.KryptonButton1 = New ComponentFactory.Krypton.Toolkit.KryptonButton()
+        Me.Button2 = New ComponentFactory.Krypton.Toolkit.KryptonButton()
+        Me.Button1 = New ComponentFactory.Krypton.Toolkit.KryptonButton()
+        Me.CheckBox1 = New ComponentFactory.Krypton.Toolkit.KryptonCheckBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.ForeColor = System.Drawing.Color.Gray
+        Me.TextBox1.Location = New System.Drawing.Point(12, 30)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ShortcutsEnabled = False
+        Me.TextBox1.Size = New System.Drawing.Size(177, 24)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "Username"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(98, 57)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(91, 22)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Preview:"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 108)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(263, 88)
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'SaveFileDialog1
+        '
+        Me.SaveFileDialog1.DefaultExt = "png"
+        Me.SaveFileDialog1.Filter = "Minecraft Skin (.png)|*.png"
+        Me.SaveFileDialog1.Title = "Save Skin"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Panel1.Controls.Add(Me.PictureBox2)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.KryptonButton1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(287, 24)
+        Me.Panel1.TabIndex = 6
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackgroundImage = CType(resources.GetObject("PictureBox2.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox2.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(24, 24)
+        Me.PictureBox2.TabIndex = 10
+        Me.PictureBox2.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.LabelStyle = ComponentFactory.Krypton.Toolkit.LabelStyle.BoldPanel
+        Me.Label2.Location = New System.Drawing.Point(30, 3)
+        Me.Label2.Name = "Label2"
+        Me.Label2.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Office2010Black
+        Me.Label2.Size = New System.Drawing.Size(146, 20)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Values.Text = "Minecraft Skin Grabber"
+        '
+        'KryptonButton1
+        '
+        Me.KryptonButton1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.KryptonButton1.Dock = System.Windows.Forms.DockStyle.Right
+        Me.KryptonButton1.Location = New System.Drawing.Point(263, 0)
+        Me.KryptonButton1.Name = "KryptonButton1"
+        Me.KryptonButton1.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.SparkleOrange
+        Me.KryptonButton1.Size = New System.Drawing.Size(24, 24)
+        Me.KryptonButton1.TabIndex = 10
+        Me.KryptonButton1.Values.Text = "X"
+        '
+        'Button2
+        '
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button2.Location = New System.Drawing.Point(90, 202)
+        Me.Button2.Name = "Button2"
+        Me.Button2.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.SparkleBlue
+        Me.Button2.Size = New System.Drawing.Size(104, 25)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Values.Text = "Save Skin"
+        '
+        'Button1
+        '
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.Location = New System.Drawing.Point(200, 30)
+        Me.Button1.Name = "Button1"
+        Me.Button1.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.SparkleBlue
+        Me.Button1.Size = New System.Drawing.Size(75, 25)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Values.Text = "Find Skin"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.Location = New System.Drawing.Point(90, 82)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.SparkleBlue
+        Me.CheckBox1.Size = New System.Drawing.Size(109, 20)
+        Me.CheckBox1.TabIndex = 9
+        Me.CheckBox1.Values.Text = "Enlarge Preview"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.ClientSize = New System.Drawing.Size(287, 239)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(293, 245)
+        Me.MinimumSize = New System.Drawing.Size(293, 245)
+        Me.Name = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button2 As ComponentFactory.Krypton.Toolkit.KryptonButton
+    Friend WithEvents Button1 As ComponentFactory.Krypton.Toolkit.KryptonButton
+    Friend WithEvents CheckBox1 As ComponentFactory.Krypton.Toolkit.KryptonCheckBox
+    Friend WithEvents KryptonButton1 As ComponentFactory.Krypton.Toolkit.KryptonButton
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Label2 As ComponentFactory.Krypton.Toolkit.KryptonLabel
+End Class
---
+++ MinecraftSkinGrabber/Form1.vb
@@ -0,0 +1,293 @@
+﻿Imports System.IO
+Imports System.Net
+Imports Newtonsoft.Json.Linq
+
+Public Class Form1
+    Dim request As HttpWebRequest
+    Dim response As HttpWebResponse
+    Dim reader As StreamReader
+    Dim rawresp As String
+    Dim uid As String
+    Dim base64 As String
+    Dim base64Decoded As String
+    Dim data() As Byte
+    Dim SkinLink As String
+    Dim reqlink As String
+    Dim drag As Boolean
+    Private draggable As Boolean
+    Private mouseX As Integer
+    Private mouseY As Integer
+
+    Private Sub SaveFileDialog1_FileOk(ByVal sender As System.Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles SaveFileDialog1.FileOk
+        If HaveInternetConnection() Then
+            If File.Exists(SaveFileDialog1.FileName) Then
+                File.Delete(SaveFileDialog1.FileName)
+            End If
+            My.Computer.Network.DownloadFile(PictureBox1.ImageLocation, SaveFileDialog1.FileName)
+            SaveFileDialog1.FileName = "Minecraft Skin"
+        Else
+            MsgBox("Could not connect to the Minecraft servers", MsgBoxStyle.Critical, "Error")
+        End If
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim parts() As String = Split(My.User.Name, "\")
+        Dim username As String = parts(1)
+        SaveFileDialog1.InitialDirectory = ("C:\users\" + username + "\Pictures\")
+        PictureBox1.SizeMode = PictureBoxSizeMode.CenterImage
+
+        With TextBox1
+            .SelectionStart = .TextLength
+            .SelectionLength = 0
+            .SelectionStart = 0
+            .ScrollToCaret()
+        End With
+
+
+
+    End Sub
+
+    Public Function HaveInternetConnection() As Boolean
+        Try
+            Return My.Computer.Network.Ping("www.minecraft.net")
+        Catch
+            Return False
+        End Try
+    End Function
+
+    Public Sub MakeRequest(reqlink)
+        request = DirectCast(WebRequest.Create(reqlink), HttpWebRequest)
+        response = DirectCast(request.GetResponse(), HttpWebResponse)
+        reader = New StreamReader(response.GetResponseStream())
+        rawresp = reader.ReadToEnd()
+    End Sub
+
+    Private Sub Button2_Click_1(sender As Object, e As EventArgs) Handles Button2.Click
+        If PictureBox1.Image Is Nothing Then
+            MsgBox("Please find a skin first", MsgBoxStyle.Critical, "Error")
+        Else
+            SaveFileDialog1.ShowDialog()
+        End If
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        If HaveInternetConnection() Then
+            If TextBox1.Text = "" Then
+                MsgBox("Please Enter A Username In The Text Box", MsgBoxStyle.Critical, "Error")
+            Else
+                Try
+                    MakeRequest("https://api.mojang.com/users/profiles/minecraft/" & TextBox1.Text)
+                    uid = JObject.Parse(rawresp)("id")
+                    MakeRequest("https://sessionserver.mojang.com/session/minecraft/profile/" & uid)
+                    base64 = JObject.Parse(rawresp)("properties")(0)("value")
+                    data = Convert.FromBase64String(base64)
+                    base64Decoded = System.Text.Encoding.ASCII.GetString(data)
+                    SkinLink = JObject.Parse(base64Decoded)("textures")("SKIN")("url")
+                    PictureBox1.ImageLocation = SkinLink
+                Catch wex As WebException
+                    Try
+                        response = DirectCast(wex.Response, HttpWebResponse)
+                        MsgBox("HTTP Code " & response.StatusCode & ". Description: " & wex.Message & " You can look up this code online for more info.", MsgBoxStyle.Information, "Web Request Error")
+                    Catch ex As Exception
+                        MsgBox("Timed out, the program will restart after this", MsgBoxStyle.Information, "Error")
+                        Application.Restart()
+                    End Try
+                Catch ex As Exception
+                    MsgBox("User does not exist or the Minecraft API is not returning a link to the skin image", MsgBoxStyle.Exclamation, "Error")
+                End Try
+            End If
+        Else
+            MsgBox("Could not connect to the Minecraft servers", MsgBoxStyle.Critical, "Error")
+        End If
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged_1(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked Then
+            PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        Else
+            PictureBox1.SizeMode = PictureBoxSizeMode.CenterImage
+        End If
+    End Sub
+
+    Private Sub KryptonButton1_Click(sender As Object, e As EventArgs) Handles KryptonButton1.Click
+        Close()
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        With TextBox1
+            If .Text = "" Then
+                .Text = "Username"
+                .ForeColor = Color.Gray
+            End If
+            If .Text = "Username" And .ForeColor = Color.Gray Then
+                .ShortcutsEnabled = False
+            Else
+                .ShortcutsEnabled = True
+            End If
+            If .TextLength > 8 Then
+                If StrReverse(StrReverse(.Text).Remove(8)) = "Username" Then
+                    .Text = .Text.Remove(.TextLength - 8)
+                    .ForeColor = Color.Black
+                    .SelectionStart = .TextLength
+                    .ScrollToCaret()
+                End If
+            End If
+        End With
+    End Sub
+
+    Private Sub TextBox1_KeyDown(sender As Object, e As KeyEventArgs) Handles TextBox1.KeyDown
+        With TextBox1
+            If .Text = "Username" And .ForeColor = Color.Gray Then
+                If e.KeyCode = Keys.Right Or e.KeyCode = Keys.Left Or e.KeyCode = Keys.Up Or e.KeyCode = Keys.Down Or e.KeyCode = Keys.Home Or e.KeyCode = Keys.End Then
+                    e.Handled = True
+                End If
+            End If
+        End With
+    End Sub
+
+    Private Sub TextBox1_MouseDown(sender As Object, e As MouseEventArgs) Handles TextBox1.MouseDown
+        drag = True
+        With TextBox1
+            If .Text = "Username" And .ForeColor = Color.Gray Then
+                .SelectionStart = .TextLength
+                .SelectionLength = 0
+                .SelectionStart = 0
+                .ScrollToCaret()
+            End If
+        End With
+    End Sub
+
+    Private Sub TextBox1_MouseMove(sender As Object, e As MouseEventArgs) Handles TextBox1.MouseMove
+        If drag Then
+            With TextBox1
+                If .Text = "Username" And .ForeColor = Color.Gray Then
+                    TextBox1.Select(0, 0)
+                End If
+            End With
+        End If
+    End Sub
+
+    Private Sub TextBox1_MouseUp(sender As Object, e As MouseEventArgs) Handles TextBox1.MouseUp
+        drag = False
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+    Private Sub Panel1_MouseDown(sender As Object, e As MouseEventArgs) Handles Panel1.MouseDown
+        draggable = True
+        mouseX = Cursor.Position.X - Me.Left
+        mouseY = Cursor.Position.Y - Me.Top
+    End Sub
+
+    Private Sub Panel1_MouseMove(sender As Object, e As MouseEventArgs) Handles Panel1.MouseMove
+        If draggable Then
+            Me.Top = Cursor.Position.Y - mouseY
+            Me.Left = Cursor.Position.X - mouseX
+        End If
+    End Sub
+
+    Private Sub Panel1_MouseUp(sender As Object, e As MouseEventArgs) Handles Panel1.MouseUp
+        draggable = False
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+    Private Sub Label1_MouseDown(sender As Object, e As MouseEventArgs) Handles Label1.MouseDown
+        draggable = True
+        mouseX = Cursor.Position.X - Me.Left
+        mouseY = Cursor.Position.Y - Me.Top
+    End Sub
+
+    Private Sub Label1_MouseMove(sender As Object, e As MouseEventArgs) Handles Label1.MouseMove
+        If draggable Then
+            Me.Top = Cursor.Position.Y - mouseY
+            Me.Left = Cursor.Position.X - mouseX
+        End If
+    End Sub
+
+    Private Sub Label1_MouseUp(sender As Object, e As MouseEventArgs) Handles Label1.MouseUp
+        draggable = False
+    End Sub
+
+    Private Sub Label2_Paint(sender As Object, e As PaintEventArgs) Handles Label2.Paint
+
+    End Sub
+
+    Private Sub Label2_MouseDown(sender As Object, e As MouseEventArgs) Handles Label2.MouseDown
+        draggable = True
+        mouseX = Cursor.Position.X - Me.Left
+        mouseY = Cursor.Position.Y - Me.Top
+    End Sub
+
+    Private Sub Label2_MouseMove(sender As Object, e As MouseEventArgs) Handles Label2.MouseMove
+        If draggable Then
+            Me.Top = Cursor.Position.Y - mouseY
+            Me.Left = Cursor.Position.X - mouseX
+        End If
+    End Sub
+
+    Private Sub Label2_MouseUp(sender As Object, e As MouseEventArgs) Handles Label2.MouseUp
+        draggable = False
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+
+    End Sub
+
+    Private Sub PictureBox2_MouseDown(sender As Object, e As MouseEventArgs) Handles PictureBox2.MouseDown
+        draggable = True
+        mouseX = Cursor.Position.X - Me.Left
+        mouseY = Cursor.Position.Y - Me.Top
+    End Sub
+
+    Private Sub PictureBox2_MouseMove(sender As Object, e As MouseEventArgs) Handles PictureBox2.MouseMove
+        If draggable Then
+            Me.Top = Cursor.Position.Y - mouseY
+            Me.Left = Cursor.Position.X - mouseX
+        End If
+    End Sub
+
+    Private Sub PictureBox2_MouseUp(sender As Object, e As MouseEventArgs) Handles PictureBox2.MouseUp
+        draggable = False
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub PictureBox1_MouseDown(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseDown
+        draggable = True
+        mouseX = Cursor.Position.X - Me.Left
+        mouseY = Cursor.Position.Y - Me.Top
+    End Sub
+
+    Private Sub PictureBox1_MouseMove(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseMove
+        If draggable Then
+            Me.Top = Cursor.Position.Y - mouseY
+            Me.Left = Cursor.Position.X - mouseX
+        End If
+    End Sub
+
+    Private Sub PictureBox1_MouseUp(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseUp
+        draggable = False
+    End Sub
+
+    Private Sub Form1_MouseDown(sender As Object, e As MouseEventArgs) Handles MyBase.MouseDown
+        draggable = True
+        mouseX = Cursor.Position.X - Me.Left
+        mouseY = Cursor.Position.Y - Me.Top
+    End Sub
+
+    Private Sub Form1_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+        If draggable Then
+            Me.Top = Cursor.Position.Y - mouseY
+            Me.Left = Cursor.Position.X - mouseX
+        End If
+    End Sub
+
+    Private Sub Form1_MouseUp(sender As Object, e As MouseEventArgs) Handles Me.MouseUp
+        draggable = False
+    End Sub
+End Class
